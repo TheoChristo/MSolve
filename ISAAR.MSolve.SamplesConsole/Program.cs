4c4aa8eceb60946f6d85736cfe66a91f4dd8ffaf
Odysseas Kokkinos <odyred@gmail.com>
2016-09-22 14:23:46 +0300

    Added a stochastic VRF example

--- ISAAR.MSolve.SamplesConsole/Program.cs
+++ ISAAR.MSolve.SamplesConsole/Program.cs
@@ -17,10 +17,10 @@ namespace ISAAR.MSolve.SamplesConsole
         {
             VectorExtensions.AssignTotalAffinityCount();
             Model model = new Model();
-            Model model_1 = new Model();
             model.SubdomainsDictionary.Add(1, new Subdomain() { ID = 1 });
             BeamBuildingBuilder.MakeBeamBuilding(model, 20, 20, 20, 5, 4, model.NodesDictionary.Count + 1,
                 model.ElementsDictionary.Count + 1, 1, 4, false, false);
+            model.Loads.Add(new Load() { Amount = -100, Node = model.Nodes[21], DOF = DOFType.X });
             model.ConnectDataStructures();
 
             SolverSkyline solver = new SolverSkyline(model);
@@ -35,10 +35,60 @@ namespace ISAAR.MSolve.SamplesConsole
             parentAnalyzer.Solve();
         }
 
+        private static void SolveBuildingInNoSoilSmallVRFStochastic()
+        {
+            VectorExtensions.AssignTotalAffinityCount();
+            Model model = new Model();
+            model.SubdomainsDictionary.Add(1, new Subdomain() { ID = 1 });
+            BeamBuildingBuilder.MakeBeamBuilding(model, 20, 20, 20, 5, 4, model.NodesDictionary.Count + 1,
+                model.ElementsDictionary.Count + 1, 1, 4, false, false);
+            model.Loads.Add(new Load() { Amount = -100, Node = model.Nodes[21], DOF = DOFType.X });
+            model.ConnectDataStructures();
+
+            PowerSpectrumTargetEvaluatorCoefficientsProvider stochasticProvider = new PowerSpectrumTargetEvaluatorCoefficientsProvider(10, 0.1, 1.2, 20, 200, DOFType.X,
+                0.1, 200, 1e-10);
+
+
+            SolverSkyline solver = new SolverSkyline(model);
+            ProblemStructural provider = new ProblemStructural(model, solver.SubdomainsDictionary);
+            LinearAnalyzer analyzer = new LinearAnalyzer(solver, solver.SubdomainsDictionary);
+            StaticAnalyzer childAnalyzer = new StaticAnalyzer(provider, analyzer, solver.SubdomainsDictionary);
+            VRFMonteCarloAnalyzerWithStochasticMaterial stochasticAnalyzer = new VRFMonteCarloAnalyzerWithStochasticMaterial(model, provider, childAnalyzer, solver.SubdomainsDictionary,
+                stochasticProvider, stochasticProvider, 1, 20, "monteCarlo");
+
+
+            analyzer.LogFactories[1] = new LinearAnalyzerLogFactory(new int[] { 420 });
+
+            //stochasticAnalyzer.BuildMatrices();
+            //childAnalyzer.Initialize();
+            stochasticAnalyzer.Solve();
+        }
+
+        private static void SolveBuildingInNoSoilSmallDynamic()
+        {
+            VectorExtensions.AssignTotalAffinityCount();
+            Model model = new Model();
+            model.SubdomainsDictionary.Add(1, new Subdomain() { ID = 1 });
+            BeamBuildingBuilder.MakeBeamBuilding(model, 20, 20, 20, 5, 4, model.NodesDictionary.Count + 1,
+                model.ElementsDictionary.Count + 1, 1, 4, false, false);
+            model.ConnectDataStructures();
+
+            SolverSkyline solver = new SolverSkyline(model);
+            ProblemStructural provider = new ProblemStructural(model, solver.SubdomainsDictionary);
+            LinearAnalyzer analyzer = new LinearAnalyzer(solver, solver.SubdomainsDictionary);
+            NewmarkDynamicAnalyzer parentAnalyzer = new NewmarkDynamicAnalyzer(provider, analyzer, solver.SubdomainsDictionary, 0.5, 0.25, 0.01, 0.1);
+
+            analyzer.LogFactories[1] = new LinearAnalyzerLogFactory(new int[] { 420 });
+
+            parentAnalyzer.BuildMatrices();
+            parentAnalyzer.Initialize();
+            parentAnalyzer.Solve();
+        }
+
 
         static void Main(string[] args)
         {
-            SolveBuildingInNoSoilSmall();
+            SolveBuildingInNoSoilSmallVRFStochastic();
         }
     }
 }
