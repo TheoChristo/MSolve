c381e429d32c99c5aeb54ede4d09993bed5e48d3
MARIA-PC\maria <mariataylaki@gmail.com>
2016-10-09 01:44:11 +0300

    Create classes ElasticityTensorContinuum3D, StressStrainVectorContinuum3D
    Class ElasticityTensorContinuum3D inherits from Matrix2D and applies the constraint that the matrix dimensions are 6 by 6
    Class StressStrainVectorContinuum3D inherits from Vector and applies the constraint that the vector dimensions is 6

---
+++ ISAAR.MSolve.Matrices/ElasticityTensorContinuum3D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.Matrices
+{
+    public class ElasticityTensorContinuum3D : Matrix2D<Double>
+    {
+        public ElasticityTensorContinuum3D() : base(6, 6)
+        {
+            // question: after I create a Matrix2D I cannot change its dimensions, right? otherwise creating this class is futile
+        }
+    }
+}
---
+++ ISAAR.MSolve.Matrices/StressStrainVectorContinuum3D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.Matrices
+{
+    public class StressStrainVectorContinuum3D : Vector<Double>
+    {
+        public StressStrainVectorContinuum3D() : base(6) { }
+    }
+}
