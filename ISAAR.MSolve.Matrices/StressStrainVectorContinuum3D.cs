67de6535b76dcab98536c0d60631658adf2d4f4e
MARIA-PC\maria <mariataylaki@gmail.com>
2016-10-09 16:29:24 +0300

    Add a constructor to StressStrainVectorContinuum3D that accepts as input an array

--- ISAAR.MSolve.Matrices/StressStrainVectorContinuum3D.cs
+++ ISAAR.MSolve.Matrices/StressStrainVectorContinuum3D.cs
@@ -8,5 +8,9 @@ namespace ISAAR.MSolve.Matrices
     public class StressStrainVectorContinuum3D : Vector<Double>
     {
         public StressStrainVectorContinuum3D() : base(6) { }
+        public StressStrainVectorContinuum3D(double[] data) : base(data)
+        {
+            if (data.Length != 6) throw new ArgumentException(String.Format("input array dimension ({0}) is not 6", data.Length));
+        }
     }
 }
