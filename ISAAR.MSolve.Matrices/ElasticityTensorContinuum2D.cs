1590ed85513528c694e571b20dc210ffc5d19280
MARIA-PC\maria <mariataylaki@gmail.com>
2016-10-09 17:29:18 +0300

    Create classes ElasticityTensorContinuum2D, StressStrainVectorContinuum2D
    Class ElasticityTensorContinuum2D inherits from Matrix2D and applies the constraint that the matrix dimensions are 3 by 3
    Class StressStrainVectorContinuum2D inherits from Vector and applies the constraint that the vector dimensions is 3

---
+++ ISAAR.MSolve.Matrices/ElasticityTensorContinuum2D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.Matrices
+{
+    public class ElasticityTensorContinuum2D : Matrix2D<Double>
+    {
+        public ElasticityTensorContinuum2D() : base(3, 3)
+        {
+            // question: after I create a Matrix2D I cannot change its dimensions, right? otherwise creating this class is futile
+        }
+
+        public ElasticityTensorContinuum2D(double[,] data) : base(data)
+        {
+            if (data.GetLength(0) != 3 || data.GetLength(1) != 3) throw new ArgumentException(String.Format("input array (dimensions: {0} by {1}) is not 3 by 3", data.GetLength(0), data.GetLength(1)));
+        }
+    }
+}
