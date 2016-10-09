1e4487cc6037103f68e5bf4d3cf0308ea08d5ba0
MARIA-PC\maria <mariataylaki@gmail.com>
2016-10-09 15:28:01 +0300

    Add constructor to ElasticityTensorContinuum3D that accepts as input a 2d array

--- ISAAR.MSolve.Matrices/ElasticityTensorContinuum3D.cs
+++ ISAAR.MSolve.Matrices/ElasticityTensorContinuum3D.cs
@@ -11,5 +11,10 @@ namespace ISAAR.MSolve.Matrices
         {
             // question: after I create a Matrix2D I cannot change its dimensions, right? otherwise creating this class is futile
         }
+
+        public ElasticityTensorContinuum3D(double[,] data) : base(data)
+        {
+            if (data.GetLength(0) != 6 || data.GetLength(1) != 6) throw new ArgumentException(String.Format("input array (dimensions: {0} by {1}) is not 6 by 6", data.GetLength(0), data.GetLength(1)));
+        }
     }
 }
