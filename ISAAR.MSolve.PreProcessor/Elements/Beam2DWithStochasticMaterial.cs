ed8975a1cb4c562345866ed245727130e3921f93
Odysseas Kokkinos <odyred@gmail.com>
2016-10-18 12:05:15 +0300

    Beam2DWithStochasticMaterial initialization

---
+++ ISAAR.MSolve.PreProcessor/Elements/Beam2DWithStochasticMaterial.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.PreProcessor.Elements
+{
+    class Beam2DWithStochasticMaterial
+    {
+    }
+}
